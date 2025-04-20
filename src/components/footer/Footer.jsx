be9ac59a1c3588bed0ea8e14ffa519ea0ebf3662
FarhanaMou55 <farhanamou828@gmail.com>
2025-04-20 22:17:04 +0600

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,9 +11,9 @@ const Footer = () => {
     return (
         <div>
             <footer className="footer footer-horizontal footer-center  bg-gradient-to-r from-orange-500 via-pink-700 to-yellow-500 text-white rounded p-10">
-              
-                   <div className='w-10/12 mx-auto '>
-                   <div >
+
+                <div className='w-10/12 mx-auto '>
+                    <div >
                         <h1 className='uppercase text-md font-semibold text-base'>Quick Links</h1>
                     </div>
                     <div className="grid grid-flow-col gap-4 ">
@@ -23,25 +23,25 @@ const Footer = () => {
                         <a href="/testimonials" className="link link-hover">Testimonials</a>
                         <a href="/contacts" className="link link-hover">Contact</a>
                     </div>
-               
-                <nav>
-                    <div className="flex flex-col gap-4">
-                        <div className='gap-4'>
-                            <h1 className='uppercase text-md font-semibold text-base'>Find me in</h1>
+
+                    <nav>
+                        <div className="flex flex-col gap-4">
+                            <div className='gap-4'>
+                                <h1 className='uppercase text-md font-semibold text-base'>Find me in</h1>
+                            </div>
+                            <div className='flex items-center justify-center gap-7 '>
+                                <a href="https://www.facebook.com/profile.php?id=100090152457744" target="_blank" rel="noopener noreferrer"><FaSquareFacebook className='hover:text-white text-3xl' /></a>
+                                <a href="https://www.instagram.com/farhanamou_828/" target="_blank" rel="noopener noreferrer"><FaSquareInstagram className='hover:text-white text-3xl' /></a>
+                                <a href="https://www.linkedin.com/in/farhanamou828/" target="_blank" rel="noopener noreferrer"><FaLinkedin className='hover:text-white text-3xl' /></a>
+                                <a href="https://x.com/FarhanaIslamMo1" target="_blank" rel="noopener noreferrer"><FaSquareXTwitter className='hover:text-white text-3xl' /></a>
+                                <a href="https://wa.me/message/6EHGGUHMSFZFG1" target="_blank" rel="noopener noreferrer"><FaSquareWhatsapp className='hover:text-white text-3xl' /></a>
+                            </div>
                         </div>
-                        <div className='flex items-center justify-center gap-7 '>
-                            <a href="https://www.facebook.com/profile.php?id=100090152457744" target="_blank" rel="noopener noreferrer"><FaSquareFacebook className='hover:text-white text-3xl' /></a>
-                            <a href="https://www.instagram.com/farhanamou_828/" target="_blank" rel="noopener noreferrer"><FaSquareInstagram className='hover:text-white text-3xl' /></a>
-                            <a href="https://www.linkedin.com/in/farhanamou828/" target="_blank" rel="noopener noreferrer"><FaLinkedin className='hover:text-white text-3xl' /></a>
-                            <a href="https://x.com/FarhanaIslamMo1" target="_blank" rel="noopener noreferrer"><FaSquareXTwitter className='hover:text-white text-3xl' /></a>
-                            <a href="https://wa.me/message/6EHGGUHMSFZFG1" target="_blank" rel="noopener noreferrer"><FaSquareWhatsapp className='hover:text-white text-3xl' /></a>
-                        </div>
-                    </div>
-                </nav>
-                <aside >
-                    <p className='font-bold mt-2'>Copyright © {new Date().getFullYear()} - All right reserved by FARHANA ISLAM MOU</p>
-                </aside>
-                   </div>
+                    </nav>
+                    <aside >
+                        <p className='font-bold mt-2'>Copyright © {new Date().getFullYear()} - All right reserved by FARHANA ISLAM MOU</p>
+                    </aside>
+                </div>
             </footer>
         </div>
     );
